af53ac84815d178c96fb57c2a064c754782b29ff
saufth <saufth@gmail.com>
2023-10-18 13:15:17 -0600

    font variable added

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,5 +3,6 @@ import { IBM_Plex_Mono as IBMPlexMono } from 'next/font/google'
 export const fontPrimary = IBMPlexMono({
   weight: ['100', '200', '300', '400', '600', '600', '700'],
   subsets: ['latin'],
+  variable: '--font-primary',
   display: 'swap'
 })
