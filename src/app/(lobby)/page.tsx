01c936e0cd82444bba3603718d0e16c749b3946b
saufth <saufth@gmail.com>
2023-10-24 13:46:33 -0600

    index-page fixed

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -151,9 +151,9 @@ export default function IndexPage () {
           <div className='relative overflow-hidden'>
             <div className='bg-secondary relative z-20 p-6 aspect-[4/5] sm:aspect-[3/2] lg:aspect-[4/2] xl:aspect-[3/1] grid place-content-center rounded-3xl'>
               <div className='max-w-4xl h-fit space-y-4 lg:space-y-6 text-center'>
-                <div className='text-h2 pb-4 text-white'>
+                <Balancer as='div' className='text-h2 pb-4 text-white'>
                   Conoce más acerca de nosotros
-                </div>
+                </Balancer>
                 <div>
                   <Button asChild size='lg'>
                     <Link href='/nosotros'>
